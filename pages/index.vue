512a7f4c921eac73da7bf9937d7494e7a0dfc482
Maetad Sukarasud <maetad.s@siamraj.com>
2021-07-15 15:12:32 +0700

    feat: add validation

--- pages/index.vue
+++ pages/index.vue
@@ -8,95 +8,117 @@
     </p>
     <div class="columns is-mobile">
       <div class="column is-fullwidth is-two-thirds-tablet mx-auto">
-        <form @submit.prevent="onSubmit">
-          <b-field label="ชื่อ">
-            <b-input v-model="firstname" />
-          </b-field>
-          <b-field label="นามสกุล">
-            <b-input v-model="lastname" />
-          </b-field>
-          <b-field label="ร่วมทำบุญ">
-            <b-select v-model="donate" expanded>
-              <option v-for="(item, index) in donationTypes" :key="index" :value="item">
-                {{ item }}
-              </option>
-              <option value="OTHER">
-                อื่น ๆ
-              </option>
-            </b-select>
-          </b-field>
-          <b-field v-if="donate === 'OTHER'" label="โปรดระบุ">
-            <b-input v-model="donate_other" />
-          </b-field>
-          <b-field label="หลักฐานการโอนเงิน">
-            <b-upload
-              v-model="file"
-              drag-drop
-              expanded
-              class="background-upload-preview"
-              :style="preview ? `background-image: url(${preview});` : ''"
-            >
-              <section class="section">
-                <div class="content has-text-centered">
-                  <p>
-                    <b-icon
-                      icon="upload"
-                      size="is-large"
-                    />
-                  </p>
-                  <p>คลิกเพื่ออัปโหลดหลักฐานการโอนเงิน หรือลากไฟล์ใส่ได้เลย</p>
-                </div>
-              </section>
-            </b-upload>
-          </b-field>
-          <b-field label="จำนวนเงิน">
-            <b-input v-model="amount" type="numeric" />
-          </b-field>
-          <b-field>
-            <b-checkbox v-model="allowMail">
-              ยินยอมให้ส่งไปรษณีย์
-            </b-checkbox>
-          </b-field>
-          <b-notification
-            :closable="false"
-            type="is-info"
-            has-icon
-            message="ข้อมูลของคุณจะถูกใช้เพื่อการจัดส่งใบอนุโมทนาบัตรเท่านั้น<br>ไม่ได้นำเพื่อไปใช้ทำการตลาด และวิเคราะห์ข้อมูลใด ๆ"
-          />
-          <template v-if="allowMail">
-            <b-field label="ที่อยู่">
-              <b-input v-model="address" required placeholder="บ้านเลขที่ หมู่บ้าน ถนน" />
+        <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
+          <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ชื่อ">
+            <b-field label="ชื่อ" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-input v-model="firstname" />
             </b-field>
-            <b-field label="รหัสไปรษณีย์">
-              <b-input v-model="zipCode" type="numeric" required maxlength="5" />
+          </ValidationProvider>
+          <ValidationProvider v-slot="{ errors, valid }" rules="required" name="นามสกุล">
+            <b-field label="นามสกุล" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-input v-model="lastname" />
             </b-field>
-            <b-field label="ตำบล">
-              <b-select v-model="subDistrict" :disabled="subDistricts.length === 0" expanded @input="() => autoSuggestion()">
-                <option :value="null" disabled>
-                  กรุณาเลือกตำบล
+          </ValidationProvider>
+          <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ร่วมทำบุญ">
+            <b-field label="ร่วมทำบุญ" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-select v-model="donate" expanded>
+                <option v-for="(item, index) in donationTypes" :key="index" :value="item">
+                  {{ item }}
                 </option>
-                <option
-                  v-for="(name, index) in subDistricts"
-                  :key="index"
-                  :value="name"
-                >
-                  {{ name }}
+                <option value="OTHER">
+                  อื่น ๆ
                 </option>
               </b-select>
             </b-field>
-            <b-field label="อำเภอ">
-              <b-input v-model="district" type="numeric" required readonly />
+          </ValidationProvider>
+          <ValidationProvider v-if="donate === 'OTHER'" v-slot="{ errors, valid }" rules="required" name="โปรดระบุ">
+            <b-field label="โปรดระบุ" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-input v-model="donateOther" />
             </b-field>
-            <b-field label="จังหวัด">
-              <b-input v-model="province" type="numeric" required readonly />
+          </ValidationProvider>
+          <ValidationProvider v-slot="{ errors, valid }" rules="required" name="หลักฐานการโอนเงิน">
+            <b-field label="หลักฐานการโอนเงิน" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-upload
+                v-model="file"
+                drag-drop
+                expanded
+                class="background-upload-preview"
+                :style="preview ? `background-image: url(${preview});` : ''"
+              >
+                <section class="section">
+                  <div class="content has-text-centered">
+                    <p>
+                      <b-icon
+                        icon="upload"
+                        size="is-large"
+                      />
+                    </p>
+                    <p>คลิกเพื่ออัปโหลดหลักฐานการโอนเงิน หรือลากไฟล์ใส่ได้เลย</p>
+                  </div>
+                </section>
+              </b-upload>
             </b-field>
+          </ValidationProvider>
+          <ValidationProvider v-slot="{ errors, valid }" rules="required|numeric" name="จำนวนเงิน">
+            <b-field label="จำนวนเงิน" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+              <b-input v-model="amount" type="numeric" />
+            </b-field>
+          </ValidationProvider>
+          <b-notification
+            :closable="false"
+            type="is-success"
+            has-icon
+            message="ข้อมูลของคุณจะถูกใช้เพื่อการจัดส่งใบอนุโมทนาบัตรเท่านั้น<br>ไม่ได้นำเพื่อไปใช้ทำการตลาด และวิเคราะห์ข้อมูลใด ๆ"
+          />
+          <b-field>
+            <b-checkbox v-model="allowMail" type="is-success">
+              ยินยอมให้ส่งไปรษณีย์
+            </b-checkbox>
+          </b-field>
+          <template v-if="allowMail">
+            <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ที่อยู่">
+              <b-field label="ที่อยู่" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+                <b-input v-model="address" placeholder="บ้านเลขที่ หมู่บ้าน ถนน" />
+              </b-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ที่อยู่">
+              <b-field label="รหัสไปรษณีย์" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+                <b-input v-model="zipCode" type="numeric" maxlength="5" />
+              </b-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ที่อยู่">
+              <b-field label="ตำบล" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+                <b-select v-model="subDistrict" :disabled="subDistricts.length === 0" expanded @input="() => autoSuggestion()">
+                  <option :value="null" disabled>
+                    กรุณาเลือกตำบล
+                  </option>
+                  <option
+                    v-for="(name, index) in subDistricts"
+                    :key="index"
+                    :value="name"
+                  >
+                    {{ name }}
+                  </option>
+                </b-select>
+              </b-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ที่อยู่">
+              <b-field label="อำเภอ" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+                <b-input v-model="district" type="numeric" readonly />
+              </b-field>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors, valid }" rules="required" name="ที่อยู่">
+              <b-field label="จังหวัด" :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+                <b-input v-model="province" type="numeric" readonly />
+              </b-field>
+            </ValidationProvider>
           </template>
           <b-field>
-            <b-button type="is-success" native-type="submit" expanded>
+            <b-button type="is-success" @click="handleSubmit(onSubmit)">
               ส่งข้อมูล
             </b-button>
           </b-field>
-        </form>
+        </ValidationObserver>
       </div>
     </div>
   </section>
@@ -104,18 +126,34 @@
 
 <script>
 import Thai from 'thai-data'
+import { extend, ValidationObserver, ValidationProvider } from 'vee-validate'
 import { mapActions } from 'vuex'
+import { required, numeric } from 'vee-validate/dist/rules'
+
+extend('required', {
+  ...required,
+  message: 'กรุณากรอกข้อมูลให้ครบถ้วน'
+})
+
+extend('numeric', {
+  ...numeric,
+  message: 'กรุณากรอกตัวเลขเท่านั้น'
+})
 
 export default {
   name: 'HomePage',
   auth: false,
+  components: {
+    ValidationObserver,
+    ValidationProvider
+  },
   data () {
     return {
       file: null,
       firstname: null,
       lastname: null,
       donate: 'โลงศพ',
-      donate_other: null,
+      donateOther: null,
       address: null,
       amount: null,
       zipCode: null,
@@ -142,20 +180,50 @@ export default {
   },
   methods: {
     ...mapActions(['createDonation']),
-    onSubmit () {
-      this.createDonation({
-        file: this.file,
-        firstname: this.firstname,
-        lastname: this.lastname,
-        donate: this.donate === 'OTHER' ? this.donate_other : this.donate,
-        address: this.address,
-        amount: this.amount,
-        zipCode: this.zipCode,
-        subDistrict: this.subDistrict,
-        district: this.district,
-        province: this.province,
-        allowMail: this.allowMail
-      })
+    async onSubmit () {
+      try {
+        await this.createDonation({
+          file: this.file,
+          firstname: this.firstname,
+          lastname: this.lastname,
+          donate: this.donate === 'OTHER' ? this.donateOther : this.donate,
+          address: this.address,
+          amount: this.amount,
+          zipCode: this.zipCode,
+          subDistrict: this.subDistrict,
+          district: this.district,
+          province: this.province,
+          allowMail: this.allowMail
+        })
+        this.$buefy.dialog.alert({
+          title: 'บันทึกข้อมูลเรียบร้อย',
+          message: 'ขอบคุณที่ร่วมทำบุญกับเรา',
+          type: 'is-success',
+          hasIcon: true,
+          ariaRole: 'alertdialog',
+          ariaModal: true
+        })
+        this.clearForm()
+      } catch (e) {
+        this.$buefy.dialog.alert({
+          title: 'ไม่สามารถบันทึกข้อมูลได้',
+          message: 'กรุณาลองใหม่อีกครั้ง',
+          type: 'is-danger',
+          hasIcon: true,
+          ariaRole: 'alertdialog',
+          ariaModal: true
+        })
+      }
+    },
+    clearForm () {
+      this.file = null
+      this.firstname = null
+      this.lastname = null
+      this.donate = null
+      this.donateOther = null
+      this.address = null
+      this.allowMail = false
+      this.unsetAddress()
     },
     unsetAddress () {
       this.subDistrict = null
